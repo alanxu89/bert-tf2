38d4a6daaebe793e1654e3960630798bb889b77b
Xunnong Xu <xunnongxu@gmail.com>
2021-01-25 23:19:36 +0800

    code refactor

--- embedding.py
+++ embedding.py
@@ -72,3 +72,92 @@ class WordEmbedding(tf.keras.layers.Layer):
     @property
     def embedding_table(self):
         return self.embedding_table
+
+
+class TokenTypeEmbedding(tf.keras.layers.Layer):
+    def __init__(self,
+                 token_type_vocab_size,
+                 width,
+                 initializer,
+                 **kwargs):
+        super(TokenTypeEmbedding, self).__init__(**kwargs)
+        self.token_type_vocab_size = token_type_vocab_size
+        self.width = width
+        self.initializer = initializer
+
+    def build(self, input_shape):
+        input_shape = input_shape.as_list()
+        if len(input_shape) != 2:
+            raise ValueError("TokenTypeEmbedding expects a 2-dimensional input tensor "
+                             "of shape [batch, sequence], got "
+                             "{}".format(input_shape))
+        self.batch_size = input_shape[0]
+        self.seq_length = input_shape[1]
+
+        self.kernel = self.add_weight(
+            "kernel",
+            shape=[self.token_type_vocab_size, self.width],
+            initializer=self.initializer)
+        super(PositionEmbedding, self).build(input_shape)
+
+    def call(self, input):
+        # This vocab will be small so we always do one-hot here,
+        # since it is always faster for a small vocabulary.
+        # input shape: [batch_size, seq_length]
+        # output_shape: [batch_size, seq_length, width]
+
+        # [batch_size, seq_length] => [batch_size*seq_length]
+        flat_token_type_ids = tf.reshape(input, [-1])
+
+        # [batch_size*seq_length] => [batch_size*seq_length, vocab_size]
+        one_hot_ids = tf.one_hot(
+            flat_token_type_ids, depth=self.token_type_vocab_size)
+
+        # get shape [batch_size*seq_length, width]
+        token_type_embeddings = tf.matmul(one_hot_ids, self.kernel)
+
+        return tf.reshape(token_type_embeddings,
+                          [self.batch_size, self.seq_length, self.width])
+
+
+class PositionEmbedding(tf.keras.layers.Layer):
+    """create position embedding with respect to input shape """
+
+    def __init__(self,
+                 max_length,
+                 initializer,
+                 **kwargs):
+        super(PositionEmbedding, self).__init__(**kwargs)
+        if max_length is None:
+            raise ValueError("`max_length` must be an Integer, not `None`.")
+        self.max_length = max_length
+        self.initializer = initializer
+
+    def build(self, input_shape):
+        dimension_list = input_shape.as_list()
+        if len(dimension_list) != 3:
+            raise ValueError("PositionEmbedding expects a 3-dimensional input tensor "
+                             "of shape [batch, sequence, width], got "
+                             "{}".format(input_shape))
+        width = dimension_list[2]
+
+        self.position_embeddings = self.add_weight(
+            "embeddings",
+            shape=[self.max_length, width],
+            initializer=self.initializer)
+        super(PositionEmbedding, self).build(input_shape)
+
+    def call(self, inputs):
+        input_shape = tf.shape(inputs)
+        position_embeddings = self.position_embeddings[:input_shape[1], :]
+        return tf.broadcast_to(position_embeddings, input_shape)
+
+
+if __name__ == "__main__":
+    emb = PositionEmbedding(100)
+    print(emb.get_config())
+    position_embedding = PositionEmbedding(max_length=100)
+    inputs = tf.keras.Input((50, 32), dtype=tf.float32)
+    inputs = tf.constant(np.random.randn(2, 3, 4))
+    outputs = position_embedding(inputs)
+    print(outputs)
--- model.py
+++ model.py
@@ -6,6 +6,8 @@ import json
 import numpy as np
 import tensorflow as tf
 
+from embedding import WordEmbedding, PositionEmbedding, TokenTypeEmbedding
+
 
 class BertConfig:
     """Config for BERT model"""
@@ -126,10 +128,11 @@ class BertModel:
             input_mask = tf.ones(
                 shape=[batch_size, seq_length], dtype=tf.int32)
 
-        self.word_embedding = tf.keras.layers.Embedding(
+        self.word_embedding = WordEmbedding(
             config.vocab_size,
             config.embedding_size,
-            create_initializer(config.initializer_range))
+            create_initializer(config.initializer_range),
+            use_one_hot_embeddings)
 
         if token_type_ids is None:
             token_type_ids = tf.zeros(
@@ -149,94 +152,7 @@ def create_initializer(initializer_range=0.02):
     return tf.keras.initializers.TruncatedNormal(stddev=initializer_range)
 
 
-class TokenTypeEmbedding(tf.keras.layers.Layer):
-    def __init__(self,
-                 token_type_vocab_size,
-                 width,
-                 initializer,
-                 **kwargs):
-        super(TokenTypeEmbedding, self).__init__(**kwargs)
-        self.token_type_vocab_size = token_type_vocab_size
-        self.width = width
-        self.initializer = initializer
-
-    def build(self, input_shape):
-        input_shape = input_shape.as_list()
-        if len(input_shape) != 2:
-            raise ValueError("TokenTypeEmbedding expects a 2-dimensional input tensor "
-                             "of shape [batch, sequence], got "
-                             "{}".format(input_shape))
-        self.batch_size = input_shape[0]
-        self.seq_length = input_shape[1]
-
-        self.kernel = self.add_weight(
-            "kernel",
-            shape=[self.token_type_vocab_size, self.width],
-            initializer=self.initializer)
-        super(PositionEmbedding, self).build(input_shape)
-
-    def call(self, input):
-        # This vocab will be small so we always do one-hot here,
-        # since it is always faster for a small vocabulary.
-        # input shape: [batch_size, seq_length]
-        # output_shape: [batch_size, seq_length, width]
-
-        # [batch_size, seq_length] => [batch_size*seq_length]
-        flat_token_type_ids = tf.reshape(input, [-1])
-
-        # [batch_size*seq_length] => [batch_size*seq_length, vocab_size]
-        one_hot_ids = tf.one_hot(
-            flat_token_type_ids, depth=self.token_type_vocab_size)
-
-        # get shape [batch_size*seq_length, width]
-        token_type_embeddings = tf.matmul(one_hot_ids, self.kernel)
-
-        return tf.reshape(token_type_embeddings,
-                          [self.batch_size, self.seq_length, self.width])
-
-
-class PositionEmbedding(tf.keras.layers.Layer):
-    """create position embedding with respect to input shape """
-
-    def __init__(self,
-                 max_length,
-                 initializer,
-                 **kwargs):
-        super(PositionEmbedding, self).__init__(**kwargs)
-        if max_length is None:
-            raise ValueError("`max_length` must be an Integer, not `None`.")
-        self.max_length = max_length
-        self.initializer = initializer
-
-    def build(self, input_shape):
-        dimension_list = input_shape.as_list()
-        if len(dimension_list) != 3:
-            raise ValueError("PositionEmbedding expects a 3-dimensional input tensor "
-                             "of shape [batch, sequence, width], got "
-                             "{}".format(input_shape))
-        width = dimension_list[2]
-
-        self.position_embeddings = self.add_weight(
-            "embeddings",
-            shape=[self.max_length, width],
-            initializer=self.initializer)
-        super(PositionEmbedding, self).build(input_shape)
-
-    def call(self, inputs):
-        input_shape = tf.shape(inputs)
-        position_embeddings = self.position_embeddings[:input_shape[1], :]
-        return tf.broadcast_to(position_embeddings, input_shape)
-
-
 if __name__ == "__main__":
     d = {"vocab_size": 100, "hidden_size": 1024}
     config = BertConfig.from_dict(d)
     print(config.to_json_string())
-
-    emb = PositionEmbedding(100)
-    print(emb.get_config())
-    position_embedding = PositionEmbedding(max_length=100)
-    inputs = tf.keras.Input((50, 32), dtype=tf.float32)
-    inputs = tf.constant(np.random.randn(2, 3, 4))
-    outputs = position_embedding(inputs)
-    print(outputs)
